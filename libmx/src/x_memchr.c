dd3a04085a15414ceb995e2a3dcd68eb66dc5027
attapack <75312811+attapack@users.noreply.github.com>
2021-12-11 18:27:25 +0200

    Create x_memchr.c

---
+++ libmx/src/x_memchr.c
@@ -0,0 +1,13 @@
+#include "../inc/libmx.h"
+
+void *mx_memchr(const void *s, int c, size_t n){
+    char *p = (char*)s;
+
+    while (n--) {
+        if( *p == (unsigned char)c ){
+            return p;
+        }
+        p++;
+    }
+    return 0;  
+}
