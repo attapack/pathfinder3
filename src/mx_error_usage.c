82c269723e089b9e6e26ac963b7cc6879b20d76a
attapack <75312811+attapack@users.noreply.github.com>
2021-12-11 18:12:18 +0200

    Create mx_error_usage.c

---
+++ src/mx_error_usage.c
@@ -0,0 +1,9 @@
+#include "../inc/pathfinder.h"
+
+void mx_error_usage(int argc) {
+    if (argc != 2) {
+        char *error = "usage: ./pathfinder [filename]\n";
+        write(2, error, mx_strlen(error));
+        exit(1);
+    }  
+}
